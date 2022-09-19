1408309e35e9469b09138c488ae4fb04fe52dd57
kckuei <kevinkuei@gmail.com>
2022-09-19 11:59:30 -0700

    Implement preorder, inroder, postorder methods

--- lib/binary-search-trees.rb
+++ lib/binary-search-trees.rb
@@ -142,16 +142,55 @@ class BinarySearchTree
   end
 
   # Traverse the three in depth-first order (inorder) and yield each node to given block
+  # inorder: left > root > right
   # Returns an array if no block given
-  def inorder; end
+  def inorder(root = @root, array = [], &block)
+    if root.nil?
+      if block_given?
+        return
+      else
+        return array
+      end
+    end
+    inorder(root.left, array, &block)
+    block.call(root) if block_given?
+    array.push(root)
+    inorder(root.right, array, &block)
+  end
 
   # Traverse the three in depth-first order (preorder) and yield each node to given block
+  # preorder: root > left > right
   # Returns an array if no block given
+  def preorder(root = @root, array = [], &block)
+    if root.nil?
+      if block_given?
+        return
+      else
+        return array
+      end
+    end
+    block.call(root) if block_given?
+    array.push(root)
+    preorder(root.left, array, &block)
+    preorder(root.right, array, &block)
+  end
 
-  def preorder; end
   # Traverse the three in depth-first order (postorder) and yield each node to given block
+  # postorder: left > right > root
   # Returns an array if no block given
-  def postorder; end
+  def postorder(root = @root, array = [], &block)
+    if root.nil?
+      if block_given?
+        return
+      else
+        return array
+      end
+    end
+    postorder(root.left, array, &block)
+    postorder(root.right, array, &block)
+    block.call(root) if block_given?
+    array.push(root)
+  end
 
   # Given a node, return its height
   def height
@@ -227,3 +266,27 @@ tree.pretty_print
 tree.level_order { |node| puts node.key }
 level_ordering = tree.level_order
 p level_ordering.inject([]) { |acc, node| acc << node.key }
+
+puts "\nBuild new tree from sorted array, return preorder"
+array = [20, 30, 50, 40, 32, 34, 36, 70, 60, 65, 75, 80, 85]
+tree = BinarySearchTree.new(sorted_unique(array))
+tree.pretty_print
+tree.preorder { |node| puts node.key }
+preordering = tree.preorder
+p preordering.inject([]) { |acc, node| acc << node.key }
+
+puts "\nBuild new tree from sorted array, return postorder"
+array = [20, 30, 50, 40, 32, 34, 36, 70, 60, 65, 75, 80, 85]
+tree = BinarySearchTree.new(sorted_unique(array))
+tree.pretty_print
+# tree.postorder { |node| puts node.key }
+postordering = tree.postorder
+p postordering.inject([]) { |acc, node| acc << node.key }
+
+puts "\nBuild new tree from sorted array, return inorder"
+array = [20, 30, 50, 40, 32, 34, 36, 70, 60, 65, 75, 80, 85]
+tree = BinarySearchTree.new(sorted_unique(array))
+tree.pretty_print
+# tree.inorder { |node| puts node.key }
+inordering = tree.inorder
+p inordering.inject([]) { |acc, node| acc << node.key }
